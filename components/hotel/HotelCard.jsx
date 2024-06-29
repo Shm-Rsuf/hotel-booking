83b279bad0872bdfce565123feacd7c2b9481c27
Usuf Ali <usuf.iu.cse16@gmail.com>
2024-06-29 22:14:49 +0600

    hotel cart, hotel summary added

---
+++ components/hotel/HotelCard.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable @next/next/no-img-element */
+import HotelSummaryInfo from "./HotelSummaryInfo";
+
+const HotelCard = () => {
+  return (
+    <div className='flex gap-6 border border-gray/20 p-4 rounded-md'>
+      <img
+        src='./images/image-1.png'
+        className='max-h-[162px] max-w-[240px]'
+        alt=''
+      />
+      <HotelSummaryInfo fromListPage={true} />
+    </div>
+  );
+};
+
+export default HotelCard;
