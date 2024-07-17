8a20acd18810f543f0efa4d4cbb9c855ec8ba977
Usuf Ali <usuf.iu.cse16@gmail.com>
2024-07-17 23:40:14 +0600

    successfully login with facebook

--- auth.js
+++ auth.js
@@ -1,4 +1,5 @@
 import NextAuth from "next-auth";
+import FacebookProvider from "next-auth/providers/facebook";
 import GoogleProvider from "next-auth/providers/google";
 
 export const {
@@ -11,6 +12,10 @@ export const {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRECT_ID,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_SECRET_ID,
     }),
   ],
 });
