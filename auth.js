193bc3f6a657b8156a6316c2bb7a5b9f7334efe2
Usuf Ali <usuf.iu.cse16@gmail.com>
2024-07-29 19:20:27 +0600

    some changes happen

--- auth.js
+++ auth.js
@@ -1,7 +1,9 @@
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 import FacebookProvider from "next-auth/providers/facebook";
 import GoogleProvider from "next-auth/providers/google";
+import { userModel } from "./app/models/user-model";
 import clientPromise from "./database/mongoClientPromise";
 
 export const {
@@ -12,6 +14,31 @@ export const {
 } = NextAuth({
   adapter: MongoDBAdapter(clientPromise),
   providers: [
+    CredentialsProvider({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials) {
+        if (credentials === null) return null;
+
+        try {
+          const user = await userModel.findOne({ email: credentials.email });
+          if (user) {
+            const isMatch = user.email === credentials.email;
+            if (isMatch) {
+              return user;
+            } else {
+              throw new Error("user or pasword mismatch!");
+            }
+          } else {
+            throw new Error("User not found!");
+          }
+        } catch (error) {
+          throw new Error(error);
+        }
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRECT_ID,
