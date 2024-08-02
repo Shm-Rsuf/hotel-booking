7b924d8df6631a7be9e0811a52707d216a723d94
Usuf Ali <usuf.iu.cse16@gmail.com>
2024-08-02 15:35:51 +0600

    register route created

--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -1,1 +1,24 @@
+import { userModel } from "@/app/models/user-model";
+import { dbConnect } from "@/services/mongo";
+import bcrypt from "bcrypt";
+import { NextRequest, NextResponse } from "next/server";
 
+export const POST = async (req, res) => {
+  const { fname, lname, email, password } = NextRequest.json();
+
+  await dbConnect();
+  const salt = bcrypt.genSalt(10);
+  const hashedPassword = bcrypt.hash(password, salt);
+
+  const newUser = {
+    name: `${fname} ${lname}`,
+    email,
+    password: hashedPassword,
+  };
+  try {
+    await userModel.create(newUser);
+    return new NextResponse("user has been created", { status: 201 });
+  } catch (error) {
+    return new NextResponse(error.message, { status: 500 });
+  }
+};
