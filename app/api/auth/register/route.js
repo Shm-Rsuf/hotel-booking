5b7978487899fd31b5a1b5fafa5746b7a6ab89ae
Usuf Ali <usuf.iu.cse16@gmail.com>
2024-08-02 17:19:11 +0600

    user registration done

--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -1,24 +1,33 @@
 import { userModel } from "@/app/models/user-model";
 import { dbConnect } from "@/services/mongo";
-import bcrypt from "bcrypt";
-import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import { NextResponse } from "next/server";
 
-export const POST = async (req, res) => {
-  const { fname, lname, email, password } = NextRequest.json();
+export const POST = async (request) => {
+  const { fname, lname, email, password } = await request.json();
+
+  console.log(fname, lname, email, password);
 
   await dbConnect();
-  const salt = bcrypt.genSalt(10);
-  const hashedPassword = bcrypt.hash(password, salt);
+
+  const hashedPassword = await bcrypt.hash(password, 5);
 
   const newUser = {
     name: `${fname} ${lname}`,
     email,
     password: hashedPassword,
   };
+
+  console.log(newUser);
+
   try {
     await userModel.create(newUser);
-    return new NextResponse("user has been created", { status: 201 });
-  } catch (error) {
-    return new NextResponse(error.message, { status: 500 });
+    return new NextResponse("User has been created", {
+      status: 201,
+    });
+  } catch (err) {
+    return new NextResponse(err.message, {
+      status: 500,
+    });
   }
 };
