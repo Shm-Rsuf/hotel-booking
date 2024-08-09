ada35f49b9c77767b44dedc687eebbb7d9aa105c
Usuf Ali <usuf.iu.cse16@gmail.com>
2024-08-09 14:16:49 +0600

    hotelCard and HotelSummary added

--- components/hotel/HotelList.jsx
+++ components/hotel/HotelList.jsx
@@ -1,14 +1,15 @@
-import HotelCard from "./HotelCard";
 import { getAllHotels } from "@/database/queries";
+import HotelCard from "./HotelCard";
 
 const HotelList = async () => {
   const allHotels = await getAllHotels();
-  console.log(allHotels);
 
   return (
-    <div className="col-span-9">
-      <div className="space-y-4">
-       <HotelCard />
+    <div className='col-span-9'>
+      <div className='space-y-4'>
+        {allHotels.map((hotel) => (
+          <HotelCard key={hotel.id} hotelInfo={hotel} />
+        ))}
       </div>
     </div>
   );
--- components/hotel/HotelSummaryInfo.jsx
+++ components/hotel/HotelSummaryInfo.jsx
@@ -1,24 +1,39 @@
-const HotelSummaryInfo = ({fromListPage}) => {
+const HotelSummaryInfo = ({ fromListPage, info }) => {
+  const { thumbNailUrl, name, highRate, lowRate, city, propertyCategory } =
+    info;
   return (
     <>
       <div className={fromListPage ? "flex-1" : "flex-1 container"}>
-        <h2 className={fromListPage ? "font-bold text-lg" : "font-bold text-2xl"}>Effotel By Sayaji Jaipur</h2>
-        <p>📍 Kolkata</p>
-        <div className="flex gap-2 items-center my-4">
-          <div className="bg-primary w-[35px] h-[35px] rounded-sm text-white grid place-items-center font-bold">
+        <h2
+          className={fromListPage ? "font-bold text-lg" : "font-bold text-2xl"}
+        >
+          {name}
+        </h2>
+        <p>📍 {city}</p>
+        <div className='flex gap-2 items-center my-4'>
+          <div className='bg-primary w-[35px] h-[35px] rounded-sm text-white grid place-items-center font-bold'>
             5.3
           </div>
-          <span className="font-medium">Very Good</span>
+          <span className='font-medium'>Very Good</span>
           <span>232 Reviews</span>
+        </div>
+        <div>
+          <span className='bg-primary p-1 rounded-md'>
+            {propertyCategory} star property
+          </span>
         </div>
       </div>
 
-      <div className="flex flex-col gap-2 items-end justify-center">
-        <h2 className="text-2xl font-bold text-right">$124/night</h2>
-        <p className=" text-right">Per Night for 4 Rooms</p>
-        {
-          fromListPage ? (<button className="btn-primary ">Details</button>) : (<button className="btn-primary ">Book</button>)
-        }
+      <div className='flex flex-col gap-2 items-end justify-center'>
+        <h2 className='text-2xl font-bold text-right'>
+          ${Math.round((highRate + lowRate) / 10)}/night
+        </h2>
+        <p className=' text-right'>Per Night for 1 Room</p>
+        {fromListPage ? (
+          <button className='btn-primary '>Details</button>
+        ) : (
+          <button className='btn-primary '>Book</button>
+        )}
       </div>
     </>
   );
