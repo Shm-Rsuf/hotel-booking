83c1e9e1bbefac0d13f66e11e0567f9999e67580
Usuf Ali <usuf.iu.cse16@gmail.com>
2024-08-13 22:35:47 +0600

    hotel detail page modified

--- components/hotel/HotelSummaryInfo.jsx
+++ components/hotel/HotelSummaryInfo.jsx
@@ -2,7 +2,11 @@ import Link from "next/link";
 import HotelRating from "./HotelRating";
 import HotelReview from "./HotelReview";
 
-const HotelSummaryInfo = ({ fromListPage, info }) => {
+const HotelSummaryInfo = ({ fromListPage, info, checkin, checkout }) => {
+  let params = "";
+  if (checkin && checkout) {
+    params = `?checkin=${checkin}&checkout=${checkout}`;
+  }
   return (
     <>
       <div className={fromListPage ? "flex-1" : "flex-1 container"}>
@@ -15,6 +19,7 @@ const HotelSummaryInfo = ({ fromListPage, info }) => {
         <div className='flex gap-2 items-center my-4'>
           <HotelRating id={info?.id} />
           <HotelReview id={info?.id} />
+          {info?.isBooked && <span>Sold Out</span>}
         </div>
         <div>
           <span className='bg-primary p-1 rounded-md'>
@@ -29,7 +34,7 @@ const HotelSummaryInfo = ({ fromListPage, info }) => {
         </h2>
         <p className=' text-right'>Per Night for 1 Room</p>
         {fromListPage ? (
-          <Link href={`/hotels/${info?.id}`} className='btn-primary'>
+          <Link href={`/hotels/${info?.id}${params}`} className='btn-primary'>
             Details
           </Link>
         ) : (
